8c4425c6a96f9fe4c20dfd9beb4a04769be52a6b
Shipeng Feng <fsp261@gmail.com>
2015-11-19 14:12:59 +0800

    Prepare for 0.2

---
+++ Makefile
@@ -0,0 +1,4 @@
+release:
+	python setup.py register
+	python setup.py sdist upload
+	python setup.py bdist_wheel upload
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='lookup',
-    version='0.1',
+    version='0.2',
     url='https://github.com/fengsp/lookup',
     license='BSD',
     author='Shipeng Feng',
